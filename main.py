d7c0b7f6256e8eb01d96892972d7e39ebabf7f21
spyhere <gekav3@mail.ru>
2022-06-14 16:32:23 +0300

    init

---
+++ main.py
@@ -0,0 +1,84 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ContentType, Message
+
+API_TOKEN = '5581388532:AAH7nk3mp5QBCwwXz2zubxXNfcDqH9rxROM'
+CHANNEL_ID = '-1001630316640'
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: Message):
+    await message.answer("Привет!\nОтправь мне свои видеофайлы.\nМы тебе перезвоним!")
+
+
+@dp.message_handler(commands=['help'])
+async def send_help(message: Message):
+    await message.answer("Сейчас поможем")
+
+
+media = {}
+break_handler = {}
+
+
+@dp.message_handler(
+    content_types=[ContentType.VIDEO, ContentType.PHOTO, ContentType.TEXT])
+async def message_handler(message: Message):
+    global break_handler
+    global media
+
+    user_id = message.from_user.id
+    if user_id in break_handler:
+        return
+
+    if message.content_type == "text":
+        await message.answer("Ваше сообщение также должно содержать видео и/или фото")
+        return
+
+    no_caption_single_file = message.caption is None and message.media_group_id is None
+    no_caption_media_group = message.caption is None and not media.get(user_id)
+    if no_caption_single_file or no_caption_media_group:
+        break_handler[user_id] = None
+        await message.answer("Отпрявляемые материалы должны быть подписаны (автор, произведение)")
+        del break_handler[user_id]
+        return
+
+    caption = f"@{message.from_user.username}\n\n{message.caption}" if message.caption else None
+
+    if message.media_group_id:
+        if not media.get(user_id):
+            media[user_id] = types.MediaGroup()
+
+        match message.content_type:
+            case 'photo':
+                media[user_id].attach_photo(message.photo[-1].file_id, caption)
+            case 'video':
+                media[user_id].attach_video(message.video.file_id, caption)
+
+        await asyncio.sleep(0.5)
+        if media[user_id].media:
+            media_group = media[user_id]
+            media[user_id] = types.MediaGroup()
+            await bot.send_media_group(CHANNEL_ID, media_group)
+            await message.answer("Спасибо, файлы получены. Удачи!")
+            del media[user_id]
+            return
+    else:
+        match message.content_type:
+            case 'photo':
+                await bot.send_photo(CHANNEL_ID, message.photo[-1].file_id, caption)
+            case 'video':
+                await bot.send_video(CHANNEL_ID, message.photo[-1].file_id, caption=caption)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
