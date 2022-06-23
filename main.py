5134b676cabc9c8c72ef71e29d270b3d439089ae
spyhere <gekav3@mail.ru>
2022-06-23 19:20:51 +0300

    Rewrite polling to webhooks

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import os
+
+from aiogram.utils.executor import start_webhook
 from dotenv import load_dotenv
 import asyncio
 import logging
 from answers import Answers
 
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
 from aiogram.types import ContentType, Message
 
 load_dotenv()
 
 API_TOKEN = os.getenv('API_TOKEN')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
+
+# webhook settings
+WEBHOOK_HOST = os.getenv('WEBHOOK_HOST')
+WEBHOOK_PATH = f"/webhook/{os.getenv('API_TOKEN')}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# webserver settings
+WEBAPP_HOST = 'localhost'
+WEBAPP_PORT = 3001
 
 # Initialize bot and dispatcher
 bot = Bot(API_TOKEN)
@@ -110,5 +121,25 @@ async def useless_message_handler(message: Message):
     await message.answer(Answers.NO_INTEREST.value, 'HTML')
 
 
+async def on_startup(db):
+    await bot.set_webhook(WEBHOOK_URL)
+
+
+async def on_shutdown(db):
+    logging.warning('Shutting down gracefully...')
+
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+    logging.warning('Bye!')
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
